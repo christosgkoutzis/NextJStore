f48d71f8daa3a85d9aa8241d6cc8837d6056e822
christosgkoutzis <christosgkoutzis@gmail.com>
2024-02-18 14:31:39 +0200

    Implemented register and login(session) functions

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,7 +6,6 @@ import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
-import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 const Page = () => {
   return (
