f104d864bdbd360f94e3e01488e30cdbb6274f4a
christosgkoutzis <christosgkoutzis@gmail.com>
2024-05-01 13:12:04 +0300

    Improved responsiveness to details components of /products route

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -22,6 +22,8 @@ import {
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { ProductCardProps } from "@/components/ProductCard";
 import { Plus, ShoppingBag, User } from "lucide-react";
+import Image from 'next/image';
+
 
 interface DetailsSchemaProps {
   title: string;
@@ -42,14 +44,37 @@ const ProductDetails: React.FC<ProductCardProps> = ({ id, title, price, category
         <DialogTrigger asChild>
           <Button variant='link' className="text-slate-500">View details</Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>{title}</DialogTitle>
-            <DialogDescription>
+        <DialogContent className="max-w-[80vw] h-[90vh] lg:max-w-[60vw] lg:h-[80vh] flex items-stretch">
+          <div className="bg-white rounded-md p-7 h-full w-full group-hover:opacity-75 px-5 flex-1">
+            <img src={image} alt={title} className='h-full w-full hover:scale-105 transition-scale duration-300' />          
+          </div>
+          <div className="flex justify-around flex-col flex-1">
+            <DialogTitle className="text-left">
+              {title}
+            </DialogTitle>
+            <DialogDescription className="text-left">
               {description}
             </DialogDescription>
-          </DialogHeader>
-          <DetailsSchema title={title} price={price} image={image} seller={seller} category={category} />
+            <div className="flex items-stretch justify-between">
+              <div>
+                <div className="flex items-center gap-1">
+                  <User className="w-8 h-8" />
+                  <p className="text-sm text-gray-600 truncate w-full">{seller}</p>
+                </div>
+                <div className="flex items-start gap-1">
+                  <ShoppingBag className="w-8 h-8" />
+                  <p className="text-sm text-gray-600 truncate w-full">{category}</p>
+                </div>
+                <span className="text-xl">{currencyFormat(price)}</span>
+              </div>
+              <div className="self-end">
+                <Button variant='default' className="flex items-center justify-center">
+                  <Plus className="w-4 h-4 mr-1" />
+                  <p>Add to cart</p>
+                </Button>
+              </div>
+            </div>
+          </div>
         </DialogContent>
       </Dialog>
     );
@@ -59,18 +84,37 @@ const ProductDetails: React.FC<ProductCardProps> = ({ id, title, price, category
       <DrawerTrigger asChild>
         <Button variant='link' className="text-slate-500">View details</Button>
       </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{title}</DrawerTitle>
-          <DrawerDescription>
+      <DrawerContent className="flex flex-col justify-around">
+        <DrawerHeader>
+          <DrawerTitle className="text-center">{title}</DrawerTitle>
+          <DrawerDescription className="text-center">
             {description}
           </DrawerDescription>
         </DrawerHeader>
-        <DetailsSchema title={title} price={price} image={image} seller={seller} category={category} />
+        <div className="bg-white rounded-md p-7 group-hover:opacity-75 px-5 w-full h-[40vh] sm:h-[50vh] relative">
+          <img src={image} alt={title} className='hover:scale-105 transition-scale duration-300 w-full max-h-full' />          
+        </div>
         <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button variant="outline">Hide details</Button>
+          <div className="flex justify-between items-stretch">
+            <div>
+              <div className="flex items-center gap-1">
+                <User className="w-8 h-8" />
+                <p className="text-sm text-gray-600 truncate w-full">{seller}</p>
+              </div>
+              <div className="flex items-start gap-1">
+                <ShoppingBag className="w-8 h-8" />
+                <p className="text-sm text-gray-600 truncate w-full">{category}</p>
+              </div>
+            </div>
+            <span className="text-xl self-end">{currencyFormat(price)}</span>
+          </div>
+          <DrawerClose>
+            <Button variant="outline" className="w-full">Hide details</Button>
           </DrawerClose>
+          <Button variant='default' className="flex items-center justify-center">
+            <Plus className="w-4 h-4 mr-1" />
+            <p>Add to cart</p>
+          </Button>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
@@ -81,30 +125,9 @@ export default ProductDetails;
 
 const DetailsSchema: React.FC<DetailsSchemaProps> = ({ title, price, image, seller, category }) => {
   return (
-    <div className="max-w-screen-lg mx-auto px-5">
-      <div className="bg-white rounded-md p-7">
-        <img src={image} className="max-w-full max-h-full" alt={title} />
-      </div>
-      <div>
-        <div className="flex gap-5 items-center justify-around">
-          <div>
-          <div className="flex items-center gap-1">
-            <User className="w-8 h-8" />
-            <p className="text-sm text-gray-600 truncate w-full">{seller}</p>
-          </div>
-          <div className="flex items-center gap-1">
-            <ShoppingBag className="w-8 h-8" />
-            <p className="text-sm text-gray-600 truncate w-full">{category}</p>
-          </div>
-            <span className="text-xl">{currencyFormat(price)}</span>
-          </div>
-          <div>
-            <Button variant='default' className="flex items-center justify-center">
-              <Plus className="w-4 h-4 mr-1" />
-              <p>Add to cart</p>
-            </Button>
-          </div>
-        </div>
+    <div className="max-w-screen-lg mx-auto px-5 flex-grow">
+      <div className="bg-white rounded-md p-7 relative aspect-video overflow-hidden group-hover:opacity-75">
+        <Image src={image} alt={title} fill className="object-center object-cover"/>
       </div>
     </div>
   );
